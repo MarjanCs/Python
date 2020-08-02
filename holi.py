4fbf5c251e7153c94e48b87302f98b3a1986c977
Marjan Celi <marjanazliga@gmail.com>
2020-08-02 16:08:01 -0500

    avance1

--- holi.py
+++ holi.py
@@ -1,5 +1,20 @@
+def definir_matriz(cadena,matriz):
+  cont = 0 #rastrear los indeces que se han ingresado en la lista
+  lista=cadena.split(",")
+  print(lista)
+  if len(lista) == (len(matriz)*len(matriz[0])):
+    for i in range(len(matriz)):
+      #se llena desde la ultima fila hasta la primera 
+      j = len(matriz)-1
+      while (j >= 0):
+        matriz[i][j] = lista[cont]
+        j = j-1
+        cont = cont+1
+  return matriz
+
+
 n=int(input('Ingrese el tamano: '))
-while(n%2==0) or n<3:
+while(n%2==0) orpy n<3:
   print('Numero invalido')  
 
   n=int(input('Ingrese nuevamente el tamano: '))
@@ -7,38 +22,25 @@ while(n%2==0) or n<3:
 print('Conociendo la matriz')
 filas=n
 columnas=n
-matriz =[] 
+matriz = [] 
+
+a=input('Ingrese los valores separados por comas: ')
 
 for i in range (filas): 
   matriz.append([])
   for j in range (columnas):
     matriz[i].append(0)
 
-for i in range(filas):
-  for j in range(columnas):
-    matriz[i][j]=int(input("Ingrese dato en fila {}, columna {} : ".format(i,j)))
+matriz = definir_matriz(a,matriz)
+print(matriz)
 
-for i in matriz:
-  print(i)
-print()
 
-diagonalprin = []
-for i in range(0,filas):
-  diagonalprin.append(matriz[i][i])
-print('principal')
-print(diagonalprin)
 
-print()
-diagonalsecu = []
-for i in range(0,columnas):
-  diagonalsecu.append(matriz[i][(columnas-1)-i])
-print('Secundaria')
-print(diagonalsecu)
 
-print()
-a=diagonalprin[0]
-for i in range(0,len(diagonalsecu)):
-  if diagonalsecu==a:
-    a=diagonalsecu[i]
-b = diagonalprin.count(a)
-print(diagonalprin[b])
+
+
+
+
+
+
+
