2669ef2be4f21dfc2270ab28b68e0870506bbbb2
Marjan Celi <marjanazliga@gmail.com>
2020-08-02 21:06:37 -0500

    avance2

--- holi.py
+++ holi.py
@@ -13,8 +13,30 @@ def definir_matriz(cadena,matriz):
   return matriz
 
 
-n=int(input('Ingrese el tamano: '))
-while(n%2==0) orpy n<3:
+def presentar_central(matriz):
+  #se guarda los numeros de la diagonal princial en un array
+  diagonalprin = []
+ for i in range(0,filas):
+  diagonalprin.append(matriz[i][i])
+
+  #se guarda los numeros de la diagonal secundaria en un array
+  diagonalsecu = []
+ for i in range(0,columnas):
+  diagonalsecu.append(matriz[i][(columnas-1)-i])
+
+  #se encuentra el nuemero interseccion entreo los dos, osea el nro repetido el cual sera el centra
+  a=diagonalprin[0]
+ for i in range(0,len(diagonalsecu)):
+  if diagonalsecu==a:
+    a=diagonalsecu[i]
+  b = diagonalprin.count(a)
+  return diagonalprin[b]
+
+
+print('Proyecto Final PYTHON')
+
+n=int(input('Ingrese el tamaño: '))
+while(n%2==0) or n<3:
   print('Numero invalido')  
 
   n=int(input('Ingrese nuevamente el tamano: '))
@@ -34,9 +56,18 @@ for i in range (filas):
 matriz = definir_matriz(a,matriz)
 print(matriz)
 
+print('Que desea realizar en la matriz')
+opcion=int(input('1. Presentar el nro Central '))
+opcion=int(input('2. Presentar los nros en forma espiral desde el centro '))
+opcion=int(input('3. Multiplos del nro central'))
 
+if opcion==1:
+  matriz = presentar_central(matriz)
+  print (matriz)
 
+if opcion==2:
 
+if opcion==3:
 
 
 
